c28756e29224c23f78bf53b4873f33a5b1a66e8b
Sindre Sorhus <sindresorhus@gmail.com>
2016-01-29 16:15:15 +0100

    drop `sudo` check as it's not needed anymore - fixes #9

--- index.js
+++ index.js
@@ -1,54 +1,5 @@
 'use strict';
-const childProcess = require('child_process');
-const execa = require('execa');
-const stripAnsi = require('strip-ansi');
 const shellEnv = require('shell-env');
-const user = process.env.USER;
-const opts = {encoding: 'utf8'};
 
-function parseEnv(env) {
-	const pathLine = stripAnsi(env.trim()).split('\n').filter(x => /^PATH=/.test(x.trim()))[0];
-	return (pathLine && pathLine.split('=')[1]) || '';
-}
-
-function pathFromSudo() {
-	return execa('sudo', ['-Hiu', user, 'env'])
-		.then(x => parseEnv(x.stdout) || '')
-		.catch(() => '');
-}
-
-function pathFromSudoSync() {
-	try {
-		// TODO: use `execa` → https://github.com/sindresorhus/execa/issues/7
-		const stdout = childProcess.execFileSync('sudo', ['-Hiu', user, 'env'], opts);
-		return parseEnv(stdout) || '';
-	} catch (err) {
-		return '';
-	}
-}
-
-function longest(arr) {
-	return arr.reduce((a, b) => a.split(':').length > b.split(':').length ? a : b);
-}
-
-module.exports = () => {
-	if (process.platform === 'win32') {
-		return Promise.resolve(process.env.PATH);
-	}
-
-	return Promise.all([
-		shellEnv().then(x => x.PATH),
-		pathFromSudo()
-	]).then(longest);
-};
-
-module.exports.sync = () => {
-	if (process.platform === 'win32') {
-		return process.env.PATH;
-	}
-
-	return longest([
-		shellEnv.sync().PATH,
-		pathFromSudoSync()
-	]);
-};
+module.exports = () => shellEnv().then(x => x.PATH);
+module.exports.sync = () => shellEnv.sync().PATH;
