bfdde310665f4500cd617ffe20e6f313de8b1145
LitoMore <litomore@gmail.com>
2025-08-20 23:02:13 +0800

    Add tests

--- test.js
+++ test.js
@@ -7,20 +7,30 @@ test('async', async t => {
 	t.false(PATH.includes('\n'));
 });
 
-test('async with options', async t => {
-	const PATH = await shellPath({shell: '/bin/bash'});
-	t.true(PATH.includes('/usr/bin'));
-	t.false(PATH.includes('\n'));
-});
-
 test('sync', t => {
 	const PATH = shellPathSync();
 	t.true(PATH.includes('/usr/bin'));
 	t.false(PATH.includes('\n'));
 });
 
-test('sync with options', t => {
+test('async - with options', async t => {
+	const PATH = await shellPath({shell: '/bin/bash'});
+	t.true(PATH.includes('/usr/bin'));
+	t.false(PATH.includes('\n'));
+});
+
+test('sync - with options', t => {
 	const PATH = shellPathSync({shell: '/bin/bash'});
 	t.true(PATH.includes('/usr/bin'));
 	t.false(PATH.includes('\n'));
+});
+
+test('async - with custom shell throws on non-executable', async t => {
+	await t.throwsAsync(shellPath({shell: 'non-executable'}));
+});
+
+test('sync - with custom shell throws on non-executable', t => {
+	t.throws(() => {
+		shellPathSync({shell: 'non-executable'});
+	});
 });
