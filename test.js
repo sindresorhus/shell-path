c6785f86dcf2fc8b2908c9143580608f7dea54d1
LitoMore <litomore@gmail.com>
2025-08-20 22:58:49 +0800

    Add tests

--- test.js
+++ test.js
@@ -7,8 +7,20 @@ test('async', async t => {
 	t.false(PATH.includes('\n'));
 });
 
+test('async with options', async t => {
+	const PATH = await shellPath({shell: '/bin/bash'});
+	t.true(PATH.includes('/usr/bin'));
+	t.false(PATH.includes('\n'));
+});
+
 test('sync', t => {
 	const PATH = shellPathSync();
+	t.true(PATH.includes('/usr/bin'));
+	t.false(PATH.includes('\n'));
+});
+
+test('sync with options', t => {
+	const PATH = shellPathSync({shell: '/bin/bash'});
 	t.true(PATH.includes('/usr/bin'));
 	t.false(PATH.includes('\n'));
 });
