30d3374732537157cb8f21b261e2efeb80ec4846
Sindre Sorhus <sindresorhus@gmail.com>
2025-08-21 16:09:07 +0700

    Delete index.test-d.ts

--- index.test-d.ts
+++
@@ -1,7 +0,0 @@
-import {expectType} from 'tsd';
-import {shellPath, shellPathSync} from './index.js';
-
-expectType<Promise<string>>(shellPath());
-expectType<Promise<string>>(shellPath({shell: '/bin/bash'}));
-expectType<string>(shellPathSync());
-expectType<string>(shellPathSync({shell: '/bin/bash'}));
